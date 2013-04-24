32279dcafab908f8e764c8848134e03cafaababf
Rod Vagg <rod@vagg.org>
2013-04-24 17:06:44 +1000

    more botfix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path        = require('path')
   , splinksmvc  = require('splink-smvc')
   , isDev       = require('./lib/static-data').isDev
 
-require('./lib/bot')
+require('./lib/polyhackbot')()
 
 // init swig with 'root' param, this isn't done by `consolidate` but required by
 // swig if you want to reference templates from within templates
